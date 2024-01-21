b9c51a3e41e36e9558e78ba4a39be1a841c23786
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-01-21 17:25:33 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import { Toaster } from "react-hot-toast";
 import Contact from "./Components/Contact";
-import Header from "./Components/Header";
+import Header, { HeaderPhone } from "./Components/Header";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Timeline from "./Components/Timeline";
 import Footer from "./Components/Footer";
+import { useEffect, useState } from "react";
 
 
 
 function App() {
-  return (
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
+
+  useEffect(() => {
+    const resizeRatio = () => {
+      setRatio(window.innerWidth / window.innerHeight);
+    };
+
+    window.addEventListener("resize", resizeRatio);
+
+    return () => {
+      window.removeEventListener("resize", resizeRatio);
+    };
+  }, [ratio]);
+
+  return ratio < 2 ? (
     <>
-      <Header />
-      <Home />
+      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home ratio={ratio} />
       <Projects />
       <Timeline />
       <Skills />
@@ -21,6 +39,8 @@ function App() {
       <Footer />
       <Toaster />
     </>
+  ) : (
+    <em id="customMessage">Please Change the ratio to View!</em>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
 
-const Header = () => {
+const Header = ({ setMenuOpen, menuOpen }) => {
     return (
-        <nav>
-            <NavContent />
-        </nav>
+        <>
+            <nav>
+                <NavContent />
+            </nav>
+
+            <button className="navBtn" onClick={() => setMenuOpen(!menuOpen)}>
+                <AiOutlineMenu />
+            </button>
+
+        </>
     )
 }
 
-const NavContent = () => (
+export const HeaderPhone = ({ menuOpen, setMenuOpen }) => {
+    return (
+      <div className={`navPhone ${menuOpen ? "navPhoneComes" : ""}`}>
+        <NavContent setMenuOpen={setMenuOpen} />
+      </div>
+    );
+  };
+
+const NavContent = ({ setMenuOpen }) => (
     <>
         <h2>Yash Sharma</h2>
         <div>
-            <a href='#home'>Home</a>
-            <a href='#projects'>Projects</a>
-            <a href='#timeline'>Experience</a>
-            <a href='#skills'>Skills</a>
-            <a href='#contact'>Contact</a>
+            <a onClick={() => setMenuOpen(false)} href='#home'>Home</a>
+            <a onClick={() => setMenuOpen(false)} href='#projects'>Projects</a>
+            <a onClick={() => setMenuOpen(false)} href='#timeline'>Experience</a>
+            <a onClick={() => setMenuOpen(false)} href='#skills'>Skills</a>
+            <a onClick={() => setMenuOpen(false)} href='#contact'>Contact</a>
         </div>
         <a href="mailto:ys6845008@gmail.com">
             <button>Email</button>
