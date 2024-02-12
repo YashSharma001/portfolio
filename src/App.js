8edfc48fb49504e95dfb0fa99844dffc17601721
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-02-12 15:39:44 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -6,32 +6,33 @@ import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Timeline from "./Components/Timeline";
 import Footer from "./Components/Footer";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 
 
 
 function App() {
 
   const [menuOpen, setMenuOpen] = useState(false);
-  const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
+  // const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
 
-  useEffect(() => {
-    const resizeRatio = () => {
-      setRatio(window.innerWidth / window.innerHeight);
-    };
+  // useEffect(() => {
+  //   const resizeRatio = () => {
+  //     setRatio(window.innerWidth / window.innerHeight);
+  //   };
 
-    window.addEventListener("resize", resizeRatio);
+  //   window.addEventListener("resize", resizeRatio);
 
-    return () => {
-      window.removeEventListener("resize", resizeRatio);
-    };
-  }, [ratio]);
+  //   return () => {
+  //     window.removeEventListener("resize", resizeRatio);
+  //   };
+  // }, [ratio]);
 
-  return ratio < 2 ? (
+  return (
     <>
       <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Home ratio={ratio} />
+      <Home />
       <Projects />
       <Timeline />
       <Skills />
@@ -39,9 +40,7 @@ function App() {
       <Footer />
       <Toaster />
     </>
-  ) : (
-    <em id="customMessage">Please Change the ratio to View!</em>
-  );
+  )
 }
 
 export default App;
